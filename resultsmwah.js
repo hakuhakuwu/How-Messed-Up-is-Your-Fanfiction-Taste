774631888a39a0bcff900e1500475a8742a19940
hakuhakuwu <121713380+hakuhakuwu@users.noreply.github.com>
2023-01-07 22:19:34 +0800

    Update resultsmwah.js

--- resultsmwah.js
+++ resultsmwah.js
@@ -17,9 +17,9 @@ function calculateScore() {
 
     //Associates your score with what it says about you
     var meaning;
-    if (score >= 98) {
+    if (score >= 120) {
         meaning = "You do not read fanfiction."
-    } else if (score >= 90) {
+    } else if (score >= 100) {
         meaning = "You&#39ve read like. one fanfic and probably against your will."
     } else if (score >= 77) {
         meaning = "I feel like you&#39re more into fluff and coffee shop AUs"
