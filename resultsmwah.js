4cc7f802d87d355f5312a8640ec93b1ca57e92d1
hakuhakuwu <121713380+hakuhakuwu@users.noreply.github.com>
2023-02-14 12:45:25 +0800

    Update resultsmwah.js

--- resultsmwah.js
+++ resultsmwah.js
@@ -20,14 +20,14 @@ function calculateScore() {
     if (score >= 130) {
         meaning = "You do not read fanfiction."
     } else if (score >= 120) {
-        meaning = "You&#39ve read like. one fanfic and probably against your will."
+        meaning = "You&#39ve read like. one fanfic and probably against your will. Or you are incredibly pure."
     } else if (score >= 100) {
         meaning = "I feel like you&#39re more into fluff and coffee shop AUs"
      } else if (score >= 78) {
         meaning = "You&#39ve seen your fair share but probably didn&#39t try to explore <em>that</em> much. you probably just stick to bdsm at most"
        } else if (score >= 50) {
         meaning = "There&#s something a little bit off about you. It was the A/B/O wasn&#t it."    
-    } else if (score >= 45) {
+    } else if (score >= 35) {
         meaning = "God gave you the midnight hours to explore the deep blue sea of AO3"
     } else if (score >= 11) {
         meaning = "Virgin in theory, whore in practice."
