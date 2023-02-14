58052892778826ed35b5efe1db913f0fa666c449
hakuhakuwu <121713380+hakuhakuwu@users.noreply.github.com>
2023-02-14 11:20:56 +0800

    Update resultsmwah.js

--- resultsmwah.js
+++ resultsmwah.js
@@ -23,7 +23,7 @@ function calculateScore() {
         meaning = "You&#39ve read like. one fanfic and probably against your will."
     } else if (score >= 100) {
         meaning = "I feel like you&#39re more into fluff and coffee shop AUs"
-     } else if (score >= 58) {
+     } else if (score >= 78) {
         meaning = "You&#39ve seen your fair share but probably didn&#39t try to explore <em>that</em> much. you probably just stick to bdsm at most"
     } else if (score >= 45) {
         meaning = "God gave you the midnight hours to explore the deep blue sea of AO3"
